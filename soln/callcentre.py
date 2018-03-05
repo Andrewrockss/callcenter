496661dba7b8a7a329e597617c1d84a07dc0fc48
Andrewrockss <37053355+Andrewrockss@users.noreply.github.com>
2018-03-04 17:46:33 -0700

    Update callcentre.py

--- soln/callcentre.py
+++ soln/callcentre.py
@@ -1,5 +1,21 @@
 # read the first line
+  n , q = list(map(int, input().split()))
 
-# read the calls
+  # read the calls
+  calls = [int(i) for i in input().split()]
 
-# read and process each query
+  s_array , t_array = [], []
+  # read and process each query
+  for i in (range(0,q)):
+     s , t = list(map(int, input().split()))
+     s_array.append(s)
+     t_array.append(t)
+
+#print  the answer
+sorting = {} #empty dictionary
+#how many quaries between eacth s and t array
+for i in range(0, q):
+    sorting[i] = sum(calls[s_array[i] - 1:t_array[i]])
+
+for k, v in sorting.items():
+    print(v)
